c24fd4c240b712e032a77bedb0858608cae5e483
MelanX <info@melanx.de>
2021-12-29 22:59:35 +0100

    Add basic files

---
+++ src/main/java/de/melanx/excavar/Excavar.java
@@ -0,0 +1,12 @@
+package de.melanx.excavar;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Excavar.MODID)
+public class Excavar {
+
+    public static final String MODID = "excavar";
+
+    public Excavar() {
+    }
+}
