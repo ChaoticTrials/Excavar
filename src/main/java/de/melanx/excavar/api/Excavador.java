a41a829d876618541e2048cf5d9577efbc2b4f4e
MelanX <info@melanx.de>
2022-01-02 18:35:38 +0100

    Add Javadoc and more modular way to use Excavador

--- src/main/java/de/melanx/excavar/api/Excavador.java
+++ src/main/java/de/melanx/excavar/api/Excavador.java
@@ -24,15 +24,74 @@ public class Excavador {
     private final Predicate<Block> testBlock;
     private final List<BlockPos> blocksToMine = Lists.newArrayList();
     private final boolean preventToolBreaking;
+    private final boolean requiresCorrectTool;
 
+    /**
+     * Creates a new instance of Excavador
+     *
+     * @see #Excavador(BlockPos, Level, ServerPlayer, Predicate, boolean, boolean)
+     */
     public Excavador(@Nonnull BlockPos start, @Nonnull Level level, @Nonnull ServerPlayer player) {
+        this(start, level, player, block -> block == level.getBlockState(start).getBlock());
+    }
+
+    /**
+     * Creates a new instance of Excavador
+     *
+     * @see #Excavador(BlockPos, Level, ServerPlayer, Predicate, boolean, boolean)
+     */
+    public Excavador(@Nonnull BlockPos start, @Nonnull Level level, @Nonnull ServerPlayer player, @Nonnull Predicate<Block> filter) {
+        this(start, level, player, filter, ConfigHandler.requiresCorrectTool.get());
+    }
+
+    /**
+     * Creates a new instance of Excavador
+     *
+     * @see #Excavador(BlockPos, Level, ServerPlayer, Predicate, boolean, boolean)
+     */
+    public Excavador(@Nonnull BlockPos start, @Nonnull Level level, @Nonnull ServerPlayer player, @Nonnull Predicate<Block> filter, boolean requiresCorrectTool) {
+        this(start, level, player, filter, requiresCorrectTool, Excavar.getPlayerHandler().getData(player.getGameProfile().getId()).preventToolBreaking());
+    }
+
+    /**
+     * Creates a new instance of Excavador
+     *
+     * @see #Excavador(BlockPos, Level, ServerPlayer, Predicate, boolean, boolean)
+     */
+    public Excavador(@Nonnull BlockPos start, @Nonnull Level level, @Nonnull ServerPlayer player, boolean requiresCorrectTool) {
+        this(start, level, player, block -> block == level.getBlockState(start).getBlock(), requiresCorrectTool, Excavar.getPlayerHandler().getData(player.getGameProfile().getId()).preventToolBreaking());
+    }
+
+    /**
+     * Creates a new instance of Excavador
+     *
+     * @see #Excavador(BlockPos, Level, ServerPlayer, Predicate, boolean, boolean)
+     */
+    public Excavador(@Nonnull BlockPos start, @Nonnull Level level, @Nonnull ServerPlayer player, boolean requiresCorrectTool, boolean preventToolBreaking) {
+        this(start, level, player, block -> block == level.getBlockState(start).getBlock(), requiresCorrectTool, preventToolBreaking);
+    }
+
+    /**
+     * Creates a new instance of Excavador
+     *
+     * @param start               The base block position where to start searching for other {@link BlockPos}es.
+     * @param level               The {@link Level} where all the magic happens.
+     * @param player              The {@link ServerPlayer} which breaks the blocks.
+     * @param filter              The filter to check whether a {@link BlockPos} will be added to the list which positions shall be destroyed.
+     * @param requiresCorrectTool Whether the tool is a correct tool to generate drops.
+     */
+    public Excavador(@Nonnull BlockPos start, @Nonnull Level level, @Nonnull ServerPlayer player, @Nonnull Predicate<Block> filter, boolean requiresCorrectTool, boolean preventToolBreaking) {
         this.start = start;
         this.level = level;
         this.player = player;
-        this.testBlock = block -> block == this.level.getBlockState(start).getBlock();
-        this.preventToolBreaking = Excavar.getPlayerHandler().getData(player.getGameProfile().getId()).preventToolBreaking();
+        this.testBlock = filter;
+        this.preventToolBreaking = preventToolBreaking;
+        this.requiresCorrectTool = requiresCorrectTool;
     }
 
+    /**
+     * Searches for the {@link BlockPos}es to break
+     */
     public void findBlocks() {
         int limit = ConfigHandler.blockLimit.get();
         this.blocksToMine.add(this.start);
@@ -41,7 +100,7 @@ public class Excavador {
         BlockPos start = this.start;
 
         // find only start block when it's not the correct tool, but it's required via config
-        if (ConfigHandler.requiresCorrectTool.get() && !ForgeHooks.isCorrectToolForDrops(this.level.getBlockState(start), this.player)) {
+        if (this.requiresCorrectTool && !ForgeHooks.isCorrectToolForDrops(this.level.getBlockState(start), this.player)) {
             return;
         }
 
@@ -58,6 +117,13 @@ public class Excavador {
         }
     }
 
+    /**
+     * Searches for neighbor blocks
+     *
+     * @param pos       Base {@link BlockPos} from where to check the neighbors
+     * @param stepsLeft The amount of blocks to add to the list
+     * @return Remaining {@code stepsLeft} after finding all neighbors.
+     */
     public int addNeighbors(BlockPos pos, int stepsLeft) {
         int[] neighborPos = new int[]{0, -1, 1};
         for (int y : neighborPos) {
@@ -81,6 +147,11 @@ public class Excavador {
         return stepsLeft;
     }
 
+    /**
+     * Mines all the blocks
+     *
+     * @param tool The tool which will be used to mine all the blocks
+     */
     public void mine(ItemStack tool) {
         int stopAt = this.preventToolBreaking ? 2 : 1;
         for (BlockPos pos : this.blocksToMine) {
@@ -91,6 +162,9 @@ public class Excavador {
         }
     }
 
+    /**
+     * @return The list which contains all positions where the player will be mining
+     */
     public List<BlockPos> getBlocksToMine() {
         return this.blocksToMine;
     }
--- src/main/java/de/melanx/excavar/api/PlayerHandler.java
+++ src/main/java/de/melanx/excavar/api/PlayerHandler.java
@@ -14,32 +14,61 @@ public class PlayerHandler {
     private final Map<UUID, ClientData> players = Maps.newHashMap();
     private final Set<UUID> diggers = Sets.newHashSet();
 
+    /**
+     * @param id The {@link UUID} of the player which starts digging
+     */
     public void startDigging(UUID id) {
         this.diggers.add(id);
     }
 
+    /**
+     * @param id The {@link UUID} of the player which stops digging
+     */
     public void stopDigging(UUID id) {
         this.diggers.remove(id);
     }
 
+    /**
+     * Checks whether the {@code player} can mine multiple blocks.
+     * Considers the {@link ClientData}.
+     * Considers whether the {@code player} is digging.
+     */
     public boolean canDig(Player player) {
         UUID id = player.getGameProfile().getId();
         return this.players.containsKey(id) && (!this.players.get(id).requiresSneaking || player.isShiftKeyDown()) && !this.diggers.contains(id);
     }
 
+    /**
+     * Adds a player to the players which could use it.
+     *
+     * @param id   The {@link UUID} of the player
+     * @param data The {@link ClientData} of the player
+     */
     public void addPlayer(UUID id, ClientData data) {
         this.players.put(id, data);
     }
 
+    /**
+     * Removes a player from the players which could use it.
+     *
+     * @param id The {@link UUID} of the player
+     */
     public void removePlayer(UUID id) {
         this.players.remove(id);
     }
 
+    /**
+     * @param id The {@link UUID} of the player to get the data from
+     * @return The {@link ClientData} of the player.
+     */
     @Nonnull
     public ClientData getData(UUID id) {
         return this.players.getOrDefault(id, null);
     }
 
+    /**
+     * Used to store the client config in the {@link PlayerHandler}
+     */
     public record ClientData(boolean requiresSneaking, boolean preventToolBreaking) {
 
         public static final ClientData EMPTY = new ClientData(false, false);
