8618121a096c833fb916ec82c18188d714a207b3
MelanX <info@melanx.de>
2024-07-11 18:41:21 +0200

    add config for xp usage type (per block/per action)
    closes #16

--- src/main/java/de/melanx/excavar/ConfigHandler.java
+++ src/main/java/de/melanx/excavar/ConfigHandler.java
@@ -17,6 +17,7 @@ public class ConfigHandler {
 
     public static ForgeConfigSpec.IntValue blockLimit;
     public static ForgeConfigSpec.IntValue xpUsage;
+    public static ForgeConfigSpec.EnumValue<XpUsageType> xpUsageType;
     public static ForgeConfigSpec.DoubleValue hungerUsage;
     public static ForgeConfigSpec.BooleanValue requiresCorrectTool;
     public static ForgeConfigSpec.BooleanValue disableDiagonals;
@@ -29,8 +30,10 @@ public class ConfigHandler {
     public static void init(ForgeConfigSpec.Builder builder) {
         blockLimit = builder.comment("How many blocks should be mined at once?")
                 .defineInRange("blocklimit", 16, 2, 8192);
-        xpUsage = builder.comment("How much xp points will be used for one block")
+        xpUsage = builder.comment("How much xp points will be used for one block/executed action (see xpUsageType)")
                 .defineInRange("xpUsage", 0, 0, 1000);
+        xpUsageType = builder.comment("How much xp points will be used for one block")
+                .defineEnum("xpUsageType", XpUsageType.PER_ACTION);
         hungerUsage = builder.comment("How much food exhaustion will caused by one block")
                 .defineInRange("hungerUsage", 0.005, 0, 1000);
         requiresCorrectTool = builder.comment("If set to true, you need the correct tool to break the blocks.")
@@ -52,5 +55,10 @@ public class ConfigHandler {
                         "botania:terra_axe",
                         "botania:terra_pick"
                 ), s -> s instanceof String);
+    }
+
+    public enum XpUsageType {
+        PER_BLOCK,
+        PER_ACTION
     }
 }
