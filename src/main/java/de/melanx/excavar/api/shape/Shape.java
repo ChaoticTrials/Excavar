edbb195a54e21cf295e702e2e37b5a4a37fd62f7
MelanX <info@melanx.de>
2025-11-15 15:26:54 +0100

    Refactor shape classes for cleaner neighbor addition logic

--- src/main/java/de/melanx/excavar/api/shape/Shape.java
+++ src/main/java/de/melanx/excavar/api/shape/Shape.java
@@ -9,16 +9,20 @@ import java.util.List;
 
 public interface Shape {
 
+    @Deprecated(forRemoval = true)
+    default int addNeighbors(Level level, BlockPos pos, Direction side, BlockState originalState, List<BlockPos> blocksToMine, int stepsLeft) {
+        throw new UnsupportedOperationException("This method is deprecated and should not be used anymore.");
+    }
+
     /**
      * Searches for neighbor blocks
      *
-     * @param level         The {@link Level} where the blocks will be mined
-     * @param pos           Base {@link BlockPos} from where to check the neighbors
-     * @param side          The {@link Direction} the player is facing
-     * @param originalState The filter for filtering the block
-     * @param blocksToMine  A {@link List} which contains already added {@link BlockPos}'
-     * @param stepsLeft     The amount of blocks to add to the list
-     * @return Remaining {@code stepsLeft} after finding all neighbors.
+     * @param level            The {@link Level} where the blocks will be mined
+     * @param pos              Base {@link BlockPos} from where to check the neighbors
+     * @param forwardDirection The {@link Direction} the player is facing
+     * @param originalState    The filter for filtering the block
+     * @param blocksToMine     A {@link List} which contains already added {@link BlockPos}
+     * @param maxBlocks        The number of blocks to add to the list
      */
-    int addNeighbors(Level level, BlockPos pos, Direction side, BlockState originalState, List<BlockPos> blocksToMine, int stepsLeft);
+    void addNeighbors(Level level, BlockPos.MutableBlockPos pos, Direction forwardDirection, BlockState originalState, List<BlockPos> blocksToMine, int maxBlocks);
 }
