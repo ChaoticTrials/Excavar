63eac070dca3ed59e93b966f71a23c8fd1311e88
MelanX <info@melanx.de>
2026-08-30 11:49:08 +0200

    Add excavar specific tool tags

---
+++ src/main/java/de/melanx/excavar/data/DataCreator.java
@@ -0,0 +1,18 @@
+package de.melanx.excavar.data;
+
+import de.melanx.excavar.Excavar;
+import net.minecraft.data.DataGenerator;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+@EventBusSubscriber(modid = Excavar.MODID, bus = EventBusSubscriber.Bus.MOD)
+public class DataCreator {
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+
+        generator.addProvider(true, new ModItemTags(generator.getPackOutput(), event.getLookupProvider(), event.getExistingFileHelper()));
+    }
+}
